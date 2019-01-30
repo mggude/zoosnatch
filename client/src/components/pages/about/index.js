b2a8018b0c5f1bc87a8df723a47291a57735276d
Grace Gude <grace@Graces-MacBook-Pro.local>
2019-01-30 13:32:59 -0700

    updating initial files

---
+++ client/src/components/pages/about/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function About() {
+    return (
+      <Container fluid>
+        <Row>
+          <Col size="md-12">
+            <h1 class="text-center">ZooSnatch</h1>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+  
+  export default About;
