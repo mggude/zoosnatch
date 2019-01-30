c2625438012e5142e037a14210c041e03a139f67
Grace Gude <grace@Graces-MacBook-Pro.local>
2019-01-30 14:31:38 -0700

    About page

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+import About from "./pages/about";
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hello World!</h1>
+        <About />
       </div>
     );
   }
