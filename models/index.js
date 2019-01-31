b708fe5157db49be6f74db15acc518873cb3fd19
connor <connor.jaksik@gmail.com>
2019-01-31 15:29:10 -0700

    initial userauth files

--- models/index.js
+++ models/index.js
@@ -1,17 +1,3 @@
-var mongoose = require("mongoose");
-var Schema = mongoose.Schema;
-
-const characterSchema = new Schema ({
-    name: {type: String},
-    alive: {type: Boolean},
-    active: {type: Boolean},
-    staticImg: {type: String},
-    animatedImg: {type: String},
-    cagedImg: {type: String},
-    scene: []
-});
-
-const Character = mongoose.model("Character", characterSchema);
-
-module.exports = Character;
-
+module.exports = {
+    User: require("./Users")
+  };
